88a0c0aad3ade793bf58c6e168fd7c1b99c4cec2
David Dias <daviddias.p@gmail.com>
2015-07-13 14:08:03 -0700

    shim done, first batch of tests pass, more tests now :)

---
+++ src/index.js
@@ -0,0 +1,35 @@
+var multiplex = require('multiplex')
+var EventEmitter = require('events').EventEmitter
+var util = require('util')
+
+exports = module.exports = MultiplexStreamMuxer
+util.inherits(Connection, EventEmitter)
+
+function MultiplexStreamMuxer () {
+  var self = this
+
+  self.attach = function (transport, isListener, callback) {
+    return new Connection(transport, isListener ? 2 : 1)
+  }
+}
+
+function Connection (transport, startingId) {
+  var self = this
+  self.nextId = startingId
+
+  self.dialStream = function (callback) {
+    var stream = self.mpx.createStream(self.nextId)
+    self.nextId = self.nextId + 2
+    callback(null, stream)
+  }
+
+  self.mpx = multiplex(function onStream (stream, id) {
+    self.emit('stream', stream)
+  })
+
+  self.mpx.on('error', function (err) {
+    self.emit('error', err)
+  })
+
+  transport.pipe(self.mpx).pipe(transport)
+}
---
+++ tests/test.js
@@ -0,0 +1,14 @@
+var tape = require('tape')
+var tests = require('abstract-stream-muxer/tests')
+var MultiplexStreamMuxer = require('../src')
+
+var common = {
+  setup: function (t, cb) {
+    cb(null, MultiplexStreamMuxer)
+  },
+  teardown: function (t, cb) {
+    cb()
+  }
+}
+
+tests(tape, common)
